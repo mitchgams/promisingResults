0c49335311176ca962122b7ac1b51e0ca8f0787d
Mitchell Gams <mitchgams@gmail.com>
2020-11-08 14:47:42 -0600

    fixed some things

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
+/************************
+ * Added some extra stuff 
+ * hope thats ok.
+ */
 $('document').ready(() => {
     $('#begin').click(() => {
+        if(results.length !== 0) {
+            alert('please wait');
+            return;
+        }
         let num1 = $('#num1').val();
         let num2 = $('#num2').val();
         if(num1 === "" || num2 === "") {
             alert('please enter a 2 seperate numbers to begin.');
         } else startChain(num1, num2);
-    })
-})
+    });
+});
 
 let results = [];
 function startChain(num1, num2) {
     slowMath.add(num1, num2)
     .then(sum => {
         results.push(sum);
-        console.log(`6 + 2 = ${sum}`);
+        console.log(`${num1} + ${num2} = ${sum}`);
         return slowMath.multiply(sum, 2);
     })
     .then(product => {
@@ -54,6 +62,10 @@ function startChain(num1, num2) {
     .then(sum => {
         results.push(sum);
         console.log(`${results[7]} + 32 = ${sum}. this is the final result.`);
+        results = [];
     })
-    .catch(tHiSiSaErRoR => console.log(tHiSiSaErRoR));
+    .catch(tHiSiSaErRoR => {
+        console.log(tHiSiSaErRoR);
+        results = [];
+    });
 }
